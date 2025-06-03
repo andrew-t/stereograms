a82f12945cae42cd8dfb4b203af9b88a9bdff8cd
Andrew <fwf@andrewt.net>
2025-06-03 22:47:15 +0100

    small optimisations just in case they help
    probably not worth it but whatever

--- make-stereogram.js
+++ make-stereogram.js
@@ -9,9 +9,10 @@ export default async function makeStereogram({
 }) {
     const unscaledWidth = patternCanvas.width * patternScalingFactor + depthCanvas.width;
     output.width = output.offsetWidth * window.devicePixelRatio;
-    const scalingFactor = output.width  / unscaledWidth;
+    const scalingFactor = output.width / unscaledWidth;
     output.height = depthCanvas.height * scalingFactor;
-    const scaledPatternWidth = scalingFactor * patternScalingFactor * patternCanvas.width;
+    const totalPatternScale = scalingFactor * patternScalingFactor;
+    const scaledPatternWidth = totalPatternScale * patternCanvas.width;
 
     const ctx = output.getContext('2d');
     const depthData = depthCanvas.getContext('2d').getImageData(0, 0, depthCanvas.width, depthCanvas.height);
@@ -20,7 +21,7 @@ export default async function makeStereogram({
     const patternData = patternCanvas.getContext('2d').getImageData(0, 0, patternCanvas.width, patternCanvas.height);
 
     // Bit less than half because the right side looks nicer than the left
-    const centre = ~~(scalingFactor * depthCanvas.width * 0.4);
+    const centre = Math.round(scalingFactor * depthCanvas.width * 0.4);
     
     // Process the image one row at a time
     for (let y = 0; y < depthCanvas.height; ++y) {
@@ -29,10 +30,11 @@ export default async function makeStereogram({
         if (highY == lowY) continue;
 
         /** The x-values of this row */
-        const patternX = Array.from({ length:output.width }).map(() => null);
+        const patternX = Array.from({ length: output.width }).map(() => null);
 
         // render right hand side
-        for (let x = Math.round(centre); x < scalingFactor * depthCanvas.width; ++x) {
+        const end = output.width - scaledPatternWidth - 1;
+        for (let x = Math.round(centre); x < end; ++x) {
             const depth = depthAt(x / scalingFactor, y);
             const offset = depth * scaledPatternWidth;
             const targetX = x + scaledPatternWidth;
@@ -46,7 +48,7 @@ export default async function makeStereogram({
             // If there's a value one pixel to the left we can iterate from then do that
             const fromNeighbour = patternX[integerTargetX - 1];
             if (fromNeighbour !== null) {
-                patternX[integerTargetX] = (fromNeighbour + 1 / (depth * scalingFactor * patternScalingFactor)) % patternCanvas.width;
+                patternX[integerTargetX] = (fromNeighbour + 1 / (depth * totalPatternScale)) % patternCanvas.width;
                 continue;
             }
             // lastly, oh well, just pick a value
@@ -65,15 +67,22 @@ export default async function makeStereogram({
             if (offset == 0 || offset >= x) continue;
             patternX[x - offset] = patternX[x];
             // now propagate right in case there was a gap
-            for (let xx = x - offset + 1; patternX[xx] === null; ++xx)
-                patternX[xx] = (patternX[xx - 1] + 1 / (depth * scalingFactor * patternScalingFactor)) % patternCanvas.width;
+            let gradient = null;
+            for (let xx = x - offset + 1; patternX[xx] === null; ++xx) {
+                if (gradient === null) gradient = 1 / (depth * totalPatternScale);
+                patternX[xx] = (patternX[xx - 1] + gradient) % patternCanvas.width;
+            }
         }
 
         // draw pattern data per our X values
         if (tilePattern) {
+            let lastYy = null;
             for (let y = lowY; y < highY; ++y) {
-                const yy = Math.floor(y / (scalingFactor * patternScalingFactor)) % patternCanvas.height;
-                patternXToRgb(patternX, yy);
+                const yy = Math.floor(y / totalPatternScale) % patternCanvas.height;
+                if (yy != lastYy) {
+                    patternXToRgb(patternX, yy);
+                    lastYy = yy;
+                }
                 ctx.putImageData(anyRow, 0, y);
             }
         } else {
@@ -84,18 +93,22 @@ export default async function makeStereogram({
 
     /** x can be a float but y must be an integer */
     function depthAt(x, y) {
+        x = Math.round(x);
         if (x < 0) x = 0;
-        if (x > depthCanvas.width - 1) x = depthCanvas.width - 1;
-        const depth = depthData.data[(Math.round(x) + y * depthCanvas.width) * 4 + 1] / 255;
+        if (x >= depthCanvas.width) x = depthCanvas.width - 1;
+        const depth = depthData.data[(x + y * depthCanvas.width) * 4 + 1] / 255;
         return whiteDepth * depth + blackDepth * (1 - depth);
     }
 
     function patternXToRgb(patternX, y) {
-        for (let x = 0; x < output.width; ++x) for (let channel = 0; channel < 4; ++channel) {
-            const val = patternX[x];
+        for (let x = 0; x < output.width; ++x) {
+            let val = patternX[x];
             if (val === null) continue;
-            anyRow.data[x * 4 + channel] =
-                patternData.data[(y * patternCanvas.width + Math.round(val)) * 4 + channel];
+            val = Math.round(val);
+            const yy = y * patternCanvas.width;
+            for (let channel = 0; channel < 4; ++channel)
+                anyRow.data[x * 4 + channel] = patternData.data[(yy + val) * 4 + channel];
+            
         }
     }
 }
